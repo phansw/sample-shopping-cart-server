fef6434e4900c97da7f7cf89ab9450adef99d9bd
phansw <applesolame@hotmail.com>
2019-03-15 23:34:28 +0800

    Add stripe, addDateTime and addUser hooks to before create orders

--- src/services/orders/orders.hooks.js
+++ src/services/orders/orders.hooks.js
@@ -1,11 +1,14 @@
 const { authenticate } = require('@feathersjs/authentication').hooks;
+const stripe = require('../../hooks/stripe');
+const addDateTime = require('../../hooks/addDateTime');
+const addUser = require('../../hooks/addUser');
 
 module.exports = {
   before: {
-    all: [/*authenticate('jwt')*/],
+    all: [authenticate('jwt')],
     find: [],
     get: [],
-    create: [],
+    create: [stripe(), addDateTime('createdAt'), addUser('user')],
     update: [],
     patch: [],
     remove: []
