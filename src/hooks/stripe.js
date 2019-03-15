cb20c23c23654a2bffdf53dbd7dfe675ecd06adb
phansw <applesolame@hotmail.com>
2019-03-15 23:33:57 +0800

    Add stripe hook

---
+++ src/hooks/stripe.js
@@ -0,0 +1,21 @@
+const { PaymentError } = require('@feathersjs/errors');
+const stripe = require('stripe')('sk_test_qQpAIO1YXkLZovyKxwDxGFWJ');
+
+module.exports = () => {
+  return async (context) => {
+    const { stripeToken, amount } = context.data;
+
+    try {
+      await stripe.charges.create({
+        amount,
+        currency: 'usd',
+        description: 'Example charge',
+        source: stripeToken.id,
+      });
+    } catch (e) {
+      throw new PaymentError('Check card details');
+    }
+
+    return context;
+  };
+};
